a38298d2e9cff4face2998d23197efcb0d047c9a
Daniel Gelbshtein <daniel.kantorov@gmail.com>
2015-12-13 22:06:24 +0200

    added missing files

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utils
+{
+    public static Quaternion GetNewLookAtPoint(Vector3 whereTo, Vector3 currentPosition, Vector3 direction, float speed)
+    {
+        Vector3 targetDirection = whereTo - currentPosition;
+        float step = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(direction, targetDirection, step, 0f);
+        return Quaternion.LookRotation(newDirection);
+    }
+}
